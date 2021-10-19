5a43b7b330b976fa9778b96f3128103b488f3c96
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-10-19 14:47:18 -0700

    updated .gitignore

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');// importing the library
 const app = express(); // referring to the instance of express with name app
 
 //Let's write our first route
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello server'));
 
 const port = 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
