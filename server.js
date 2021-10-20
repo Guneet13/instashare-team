39702f36902d6cf226a83b4d29084c2d03ed7966
wtr-gg <78711816+wtr-gg@users.noreply.github.com>
2021-10-19 19:35:33 -0700

    -create/hide keys.js file -connect db addy -connect mongoose (promise)

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');// importing the library
+const db = require('./config/keys');
+const mongoose = require('mongoose');
 const app = express(); // referring to the instance of express with name app
 
 //Let's write our first route
 app.get('/', (req, res) => res.send('Hello server'));
+
+//Connect to db
+mongoose.connect(db.mongoURI)
+          .then(() => console.log('MongoDB connected')) //promise statement
+          .catch((err) => console.log(err) ); //promise statement
+
 
 const port = 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
