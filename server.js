4f6f1c45e512d529c66ce50370d8347c47d0eea8
wtr-gg <78711816+wtr-gg@users.noreply.github.com>
2021-10-20 20:45:34 -0700

    sub-routes and export

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const express = require('express');
 const app = express(); 
 const db = require('./config/keys');
 const mongoose = require('mongoose');
-const users = require('./routs/api/users');
-const profile = require('./routs/api/profile');
-const posts = require('./routs/api/posts');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 
 //Let's write our first route
 app.get('/', (req, res) => res.send('Hello server'));
+
+//if this path is called, to go this js --> calling to express to create routes to each folder
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 //Connect to db
 mongoose.connect(db.mongoURI)
