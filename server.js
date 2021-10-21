b3b2743707c790a0d4a1433e39b717a03ca318f1
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-10-21 16:28:56 -0700

    Revert "Revert "Subroutes and instruction created""

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = require('express');// importing the library
+// importing the library
+const express = require('express');
+// referring to the instance of express with name app
+const app = express(); 
 const db = require('./config/keys');
 const mongoose = require('mongoose');
-const app = express(); // referring to the instance of express with name app
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 
 //Let's write our first route
 app.get('/', (req, res) => res.send('Hello server'));
 
+//if this path is called, to go this js --> calling to express to create routes to each folder
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 //Connect to db
 mongoose.connect(db.mongoURI)
-          .then(() => console.log('MongoDB connected')) //promise statement
-          .catch((err) => console.log(err) ); //promise statement
+          .then(() => console.log('MongoDB connected')) 
+          //promise statement
+
+          .catch((err) => console.log(err) ); 
+          //promise statement
 
 
 const port = 5000;
